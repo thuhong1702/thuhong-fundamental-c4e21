b5f6fa4d38ea96859d2320491218b6214f08c32a
Thu Hong <Thu Hong>
2018-09-27 15:58:19 +0700

    initial commit

--- session2/hw2/abcdefg/d.py
+++ session2/hw2/abcdefg/d.py
@@ -1,2 +1,9 @@
 n = int(input("Enter a number "))
-print("x * " * n , "x")
+m = int(n / 2)
+d = int(n % 2)
+for i in range(m):
+    print(" x *", end = "")
+if d == 1:
+    print(" x")
+else:
+    print("")
