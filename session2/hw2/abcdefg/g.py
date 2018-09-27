af5390afccd5627be0ceae7fedbcbc75668bae97
Thu Hong <Thu Hong>
2018-09-27 07:18:16 +0700

    initial commit

---
+++ session2/hw2/abcdefg/g.py
@@ -0,0 +1,6 @@
+m = int(input("Enter a number for column "))
+n = int(input("Enter a number for row "))
+for i in range(m):
+    for j in range(n):
+        print("* ", end = '')
+    print("")
---
+++ session2/summ.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+r = range(n+1)
+total = sum(r)
+print(total)
+
+# t+=1
+# for i in range(1, n+1)
+# print(t)
